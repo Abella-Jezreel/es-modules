63b43a773f6f48929df8fd53e0575f8298eae407
Abella-Jezreel <teamabella@outlook.com>
2025-02-03 16:35:48 +0800

    More on ES Modules

--- response-handler.js
+++ response-handler.js
@@ -1,7 +1,12 @@
 import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const responseHandler = (req, res, next) => {
-  fs.readFile("my-page.html", "utf8", (err, data) => {
-    res.send(data);
-  });
+  //   fs.readFile("my-page.html", "utf8", (err, data) => {
+  //     res.send(data);
+  //   });
+  res.sendFile(path.join(__dirname, "my-page.html"));
 };
