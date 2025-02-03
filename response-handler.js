5abb872f445d4f9fec99a8ea6f46f80493e5e463
Abella-Jezreel <teamabella@outlook.com>
2025-02-03 16:42:24 +0800

    Node Core Modules and Promises

--- response-handler.js
+++ response-handler.js
@@ -1,12 +1,17 @@
-import fs from "fs";
+import fs from "fs/promises";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const responseHandler = (req, res, next) => {
-  //   fs.readFile("my-page.html", "utf8", (err, data) => {
-  //     res.send(data);
-  //   });
-  res.sendFile(path.join(__dirname, "my-page.html"));
+    fs.readFile('my-page.html', 'utf8')
+    .then((data) => {
+        res.send(data);
+    })
+    .catch((error) => {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    });
+//   res.sendFile(path.join(__dirname, "my-page.html"));
 };
